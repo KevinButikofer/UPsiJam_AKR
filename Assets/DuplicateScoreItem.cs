1e1422ec0057a2b92f33147970c6dc991735ed2c
Raphaël Margueron <raphael.margueron@gmail.com>
2022-11-19 16:31:06 +0100

    add sfx for addplayer + 2x

--- Assets/DuplicateScoreItem.cs
+++ Assets/DuplicateScoreItem.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class DuplicateScoreItem : MonoBehaviour
 {
+
+    private MusicController mc;
+    public AudioClip ac_2x;
+
+
+    void Start()
+    {
+        mc = GameObject.FindObjectOfType<MusicController>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
+            mc.PlaySFX(ac_2x);
             other.GetComponent<PlayerController>().score *= 2;
             Destroy(gameObject);
         }

dd02a19c400467b554e784fa9164104ca9d0274b
Raphaël Margueron <raphael.margueron@gmail.com>
2022-11-19 09:31:12 +0100

    "better" physic

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,7 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float maxSpeed = 5;
+    private float maxSpeed = 10;
 
     [SerializeField]
     private float weight = 1;
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
     private bool grounded = false;
 
     public TextMeshProUGUI scoreText;
+
+    private float randomModifier;
     
 
     // Start is called before the first frame update
@@ -26,23 +28,32 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.mass = weight;
+        // randomModifier = Random.Range(1f, 1.5f);
+        randomModifier = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = rb.velocity.y == 0;
+        grounded = Mathf.Abs(rb.velocity.y) < 1e-3;
 
         float f = Input.GetAxis("Horizontal");
-        rb.AddForce(new Vector2(f * 500 * Time.deltaTime, 0));
-        if (rb.velocity.x > maxSpeed)
-        {
-            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        rb.AddForce(new Vector2(f * 1000 * Time.deltaTime * randomModifier, 0));
+        float v = Mathf.Abs(rb.velocity.x);
+        
+        float sign;
+        if(v < 1e-4){
+            sign = 1;
         }
+        else{
+            sign = rb.velocity.x / v;
+        }
+
+        rb.velocity = new Vector2(Mathf.Min(v, maxSpeed) * sign, rb.velocity.y);
 
         if(Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            rb.AddForce(new Vector2(0, 500));
+            rb.AddForce(new Vector2(0, 1000));
         }
 
         scoreText.text = score.ToString();

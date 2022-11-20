f3d441bcb2c9ab7c8132dedf4123af0c2af21840
Amandine Evard <amandineevard@MacBook-Air-de-Amandine.local>
2022-11-20 13:08:36 +0100

    error fixed

--- Assets/EndSceneMenuManager.cs
+++ Assets/EndSceneMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class EndSceneMenuManager : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class EndSceneMenuManager : MonoBehaviour
     public TextMeshProUGUI textMeshScore;
     // Start is called before the first frame update
     void Start()
-    {
-        textMeshScore.text = "Total score: " + GameManager.collected.ToString();
-        float time = GameManager.totalTime;
-        int minutes = Mathf.FloorToInt(time / 60F);
-        int seconds = Mathf.FloorToInt(time - minutes * 60);
-        textMeshTime.text = "Total time: " + string.Format("{0:0}:{1:00}", minutes, seconds);
+    {   
+        DateTime endTime = DateTime.Now;
+    
+        int collected = GameManager.collectedLevel1+GameManager.collectedLevel2+GameManager.collectedLevel3+GameManager.collectedLevel4;
+        textMeshScore.text = "Total score: " + collected.ToString();
+        TimeSpan time = endTime-GameManager.startTime;
+        textMeshTime.text = "Total time: " + time.ToString("h'h 'm'm 's's'");
+        
     }
 
     // Update is called once per frame
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
- using UnityEngine.SceneManagement;
+using System;  
+using UnityEngine.SceneManagement;
 
 public class GameManager
 {
 
     public static int collected = 0;
+    public static int collectedLevel1 = 0;
+    public static int collectedLevel2 = 0;
+    public static int collectedLevel3 = 0;
+    public static int collectedLevel4 = 0;
+
     public static float totalTime = 0f;
+    public static float timeLevel1 = 0f;
+    public static float timeLevel2 = 0f;
+    public static float timeLevel3 = 0f;
+    public static float timeLevel4 = 0f;
+
+
+    public static DateTime startTime = DateTime.Now;
 
     private static string[] scenes = {"Niveau 1", "Niveau 2", "Niveau 3", "Niveau 3", "End Scene" };
 
@@ -16,6 +28,20 @@ public class GameManager
 
     public static void NextScene()
     {
+        if(currentScene == 0){
+            collectedLevel1 = collected;
+            collected = 0;
+        }else if(currentScene == 1){
+            collectedLevel2 = collected;
+            timeLevel2 = 
+            collected = 0;
+        }else if(currentScene == 2){
+            collectedLevel3 = collected;
+            collected = 0;
+        }else if(currentScene == 3){
+            collectedLevel4 = collected;
+            collected = 0;
+        }
         currentScene++;
         try
         {
